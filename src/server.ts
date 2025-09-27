18773e14a2bdf1eac6f8bf326fa1d391f6bfe102
Addam Cosmo <addamcosmo2000@gmail.com>
2025-09-27 13:23:38 -0300

    Trativa de erro para inserts no banco de dados

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,12 @@ import {fastify} from "fastify"
 import {db} from './database.js'
 import knex from "knex"
 import { randomUUID } from "crypto"
+import z from 'zod'
 
 const app = fastify()
 
 
-//ROTA DE TESTE
+//ROTA PARA RETORNO DOS USUARIOS CADASTRADOS
 app.get('/',(req,reply) =>{
     const filter = db.select().table('users')
     return filter
@@ -15,29 +16,30 @@ app.get('/',(req,reply) =>{
 //ROTA DE CADASTRO, PEGAR NOME,SENHA E VINCULAR AO BANCO
 app.post('/register',async (req,reply) =>{
     try {
-        const { name, email, password } =  req.body as {
-            name: string
-            email: string
-            password: string
 
-            
-        }
+        //TRATIVA TIPAGEM DOS DADOS COM ZOD
+        const createUserBodySchema = z.object({
+            name:z.string().nonempty().min(2),
+            email: z.string(),
+            password:z.string().min(4).max(20)
+        })
 
+        //PASSANDO OS ARQUIVOS PARA A VARIAVEL
+        const {name,email,password} = createUserBodySchema.parse(req.body)
+
+        //INSERT NO BANCO
         const NewUser ={
             id:randomUUID(),
             name,
             email,
             password
-
         }
 
         await db('users').insert(NewUser)
     }catch(error){
-        console.error(error)
+        return error
     }
 
-    //PENDENTE O INSERT NO BANCO
-   
 })
 
 
