4e26a1e4104e0b55b5fb9bee92a72662b6487ca3
Addam Cosmo <addamcosmo2000@gmail.com>
2025-09-27 13:48:23 -0300

    Hash de senhas adicionado

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,14 @@ import {fastify} from "fastify"
 import {db} from './database.js'
 import knex from "knex"
 import { randomUUID } from "crypto"
+import {hash} from 'bcrypt'
+//import { hash } from "crypto"
 import z from 'zod'
 
 const app = fastify()
 
+//DEFINIÇÃO DOS SALTOS PARA HAS DA SENHA
+const SALT_ROUNDS =10
 
 //ROTA PARA RETORNO DOS USUARIOS CADASTRADOS
 app.get('/',(req,reply) =>{
@@ -26,13 +30,13 @@ app.post('/register',async (req,reply) =>{
 
         //PASSANDO OS ARQUIVOS PARA A VARIAVEL
         const {name,email,password} = createUserBodySchema.parse(req.body)
-
+        const password_hash = await hash(password,SALT_ROUNDS)
         //INSERT NO BANCO
         const NewUser ={
             id:randomUUID(),
             name,
             email,
-            password
+            password:password_hash
         }
 
         await db('users').insert(NewUser)
