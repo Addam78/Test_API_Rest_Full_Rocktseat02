b0ba5a9fd7f70d59cca990ea54069827bc12285b
Addam Cosmo <addamcosmo2000@gmail.com>
2025-09-27 13:09:32 -0300

    Rota de cadastro criada , pendente adicionar cookies e tipagem dos dados

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,43 @@
 import {fastify} from "fastify"
 import {db} from './database.js'
+import knex from "knex"
+import { randomUUID } from "crypto"
 
 const app = fastify()
 
 
 //ROTA DE TESTE
 app.get('/',(req,reply) =>{
-    return reply.send('Hello world')
+    const filter = db.select().table('users')
+    return filter
 })
 
-//ROTA DE LOGIN
-app.post('/login',(req,reply) =>{
-    
+//ROTA DE CADASTRO, PEGAR NOME,SENHA E VINCULAR AO BANCO
+app.post('/register',async (req,reply) =>{
+    try {
+        const { name, email, password } =  req.body as {
+            name: string
+            email: string
+            password: string
+
+            
+        }
+
+        const NewUser ={
+            id:randomUUID(),
+            name,
+            email,
+            password
+
+        }
+
+        await db('users').insert(NewUser)
+    }catch(error){
+        console.error(error)
+    }
+
+    //PENDENTE O INSERT NO BANCO
+   
 })
 
 
