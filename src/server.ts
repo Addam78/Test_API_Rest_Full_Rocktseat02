da59fa785c6d3d95d5c909999f40fe0b5e96be82
Addam Cosmo <addamcosmo2000@gmail.com>
2025-09-27 18:58:52 -0300

    Rota de verificação certa, adicionar verificação por senha

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,8 @@ import knex from "knex"
 import { randomUUID } from "crypto"
 import {hash} from 'bcrypt'
 //import { hash } from "crypto"
-import z from 'zod'
+import z, { email } from 'zod'
+import { _email } from "zod/v4/core"
 
 const app = fastify()
 
@@ -44,6 +45,44 @@ app.post('/register',async (req,reply) =>{
         return error
     }
 
+})
+// app.post('/check-user', async (req, reply) => {
+//     try {
+//         const checkUserSchema = z.object({
+//             email: z.string().email()
+//         });
+
+//         const { email } = checkUserSchema.parse(req.body);
+
+//         const user = await db('users').where({ email }).first();
+
+//         if (user) {
+//             return { exists: true, user };
+//         } else {
+//             return { exists: false };
+//         }
+//     } catch (error) {
+//         reply.status(400).send({ error: 'Invalid request' });
+//     }
+// });
+//ROTA DE CADASTRO DE USUARIO OU DE LOGIN
+app.post('/diet',async(req,res) =>{
+    const  {name_user,email_user,pasword_user} = req.body
+    
+
+    const teste = await db('users').select().where({name:name_user,email:email_user}).first()
+    
+    if(teste){
+        return "Usario cadastrado"
+    }else{
+        return 'Usario não cadastrado'
+    }
+
+
+
+
+
+      
 })
 
 
